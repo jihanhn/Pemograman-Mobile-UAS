8bd51a4aeba983b6fd0eadcf5eeaea34af457fd0
jihanhn <88424153+jihanhn@users.noreply.github.com>
2025-01-16 12:19:07 +0700

    Add files via upload

---
+++ sleep_panda/lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const baseColor = Color(0xff20223F);
+
+const second = Color(0xff009090);
+
+const baseUrl = 'https://e375-125-165-153-22.ngrok-free.app';
---
+++ sleep_panda/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sleep_panda/screens/splash.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
